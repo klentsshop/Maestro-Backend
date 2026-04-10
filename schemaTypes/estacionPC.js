cc5ce317879e8e2eab49e428d935fdfbd3305171
Johanna <joisk026@gmail.com>
2026-04-10 08:25:43 -0500

    🚀 Socio POS: Core Completo Blindado

---
+++ schemaTypes/estacionPC.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'estacionPC',
+  title: 'Estaciones de Impresión Windows',
+  type: 'document',
+  fields: [
+    {
+      name: 'nombre',
+      title: 'Nombre de la PC / Estación',
+      type: 'string',
+      description: 'Ejemplo: Caja Principal o Barra Bar',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'categoriasVinculadas',
+      title: 'Categorías que imprime esta PC',
+      description: 'Selecciona las categorías que saldrán por el cable de esta estación',
+      type: 'array',
+      // 🚀 BISTURÍ: Esto vincula automáticamente con lo que el cliente cree
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'categoria' }] 
+        }
+      ]
+    },
+    {
+      name: 'pcFingerprint',
+      title: 'ID Único de esta PC',
+      type: 'string',
+      description: 'Identificador técnico del navegador'
+    }
+  ]
+}
